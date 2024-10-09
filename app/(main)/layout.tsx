6c0e1d666ff089acbc8b43120d859979bb8b895a
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-10-08 21:16:44 -0700

    feat: update main route's metadata for SEO and added more styling to the main tag
    Updated the metadata for the main route to enhance SEO. Also added Tailwind CSS classes for padding and margin to the main tag to improve layout styling.

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -3,8 +3,38 @@ import Navbar from '@/components/ui/navbar';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Btye size Academy',
-  description: 'Learn to code with Byte Size Academy',
+  metadataBase: new URL('https://academy.codedevils.org'),
+  title: {
+    template:
+      "%s | CodeDevils' Academy | CodeDevils @ Arizona State University",
+    default: "CodeDevils' Academy | CodeDevils @ Arizona State University",
+  },
+  description:
+    "Explore a wide range of coding courses designed for beginners to advanced learners at CodeDevils' Academy, part of Arizona State University.",
+
+  openGraph: {
+    title: "CodeDevils' Academy | CodeDevils @ Arizona State University",
+    description:
+      "Explore a wide range of coding courses designed for beginners to advanced learners at CodeDevils' Academy, part of Arizona State University.",
+    type: 'website',
+    url: 'https://academy.codedevils.org',
+    locale: 'en_US',
+    siteName: "CodeDevils' Academy",
+    images: [
+      {
+        url: 'https://www.codedevils.org/og.png',
+        width: 1362,
+        height: 482,
+        alt: "CodeDevils' openGraph image",
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "CodeDevils' Academy | CodeDevils @ Arizona State University",
+    description:
+      "Explore a wide range of coding courses designed for beginners to advanced learners at CodeDevils' Academy, part of Arizona State University.",
+  },
 };
 
 export default function MainLayout({
