5e5dda07baaa4ea3de4777b3bc3a258f416c9f4f
David Nevarez <69279785+SloppyFlipFlop@users.noreply.github.com>
2024-10-10 20:40:17 -0700

    feat: add banner component for navbar with role-based quick action links
    Created a banner component for the navbar that displays relevant quick action links based on the user's role and permissions.
    
    refs: [FEAT]: Navbar #1

--- components/ui/banner.tsx
+++ components/ui/banner.tsx
@@ -3,18 +3,18 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 import { useUserRole } from '@/lib/utils';
+import { Button } from './button';
 
 const Banner = () => {
   const userRole = useUserRole();
   const pathname = usePathname();
 
   const role = userRole?.split(':')[1];
-  // const role = 'student'; // for testing
   const adminList = ['admin', 'super_user', 'instructor'];
 
   return (
     <div className={'block w-full bg-gray shadow-sm'}>
-      <div className='mx-auto max-w-screen-2xl py-2'>
+      <div className='mx-auto max-w-screen-2xl py-2 px-6'>
         <ul
           className={`flex  ${
             adminList?.includes(`${role}`) ? 'justify-end' : 'justify-start'
@@ -24,37 +24,35 @@ const Banner = () => {
             <>
               {pathname === '/dashboard/instructor' ? (
                 <li>
-                  <Link
-                    href='/courses'
-                    className='bg-maroon text-white p-2 rounded uppercase md:hover:bg-maroon/80  transition-colors duration-300 text-xs'
-                  >
-                    Exit Instructor Mode
-                  </Link>
+                  <Button size={'sm'}>
+                    <Link href='/courses' className='uppercase'>
+                      Exit Instructor Mode
+                    </Link>
+                  </Button>
                 </li>
               ) : (
                 <li>
-                  <Link
-                    href='/dashboard/instructor'
-                    className='bg-maroon text-white p-2 rounded uppercase md:hover:bg-maroon/80  transition-colors duration-300 text-xs'
-                  >
-                    Instructor Mode
-                  </Link>
+                  <Button size={'sm'}>
+                    <Link href='/dashboard/instructor' className='uppercase'>
+                      Instructor Mode
+                    </Link>
+                  </Button>
                 </li>
               )}
             </>
           ) : (
             <>
               <li>
-                <Link href='/career-services' className='hover:underline'>
-                  Career Services
+                <Link href='/career-center' className='*:p-0'>
+                  <Button variant={'link'}>Career Services</Button>
                 </Link>
               </li>
               <li>
                 <Link
                   href='https://www.codedevils.io/support'
-                  className='hover:underline'
+                  className='*:p-0'
                 >
-                  Get Support
+                  <Button variant={'link'}>Get Support </Button>
                 </Link>
               </li>
             </>
