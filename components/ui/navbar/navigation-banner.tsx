c92fce9cf132ffc1d3b90601f68a6a4489d6cf28
Fernando David Nevarez <69279785+fernandonevarez@users.noreply.github.com>
2024-12-08 17:38:31 -0700

    refactor: updates navigation banner file and component structure
    Renames the navigation banner file and updates component names to reflect recent changes. Adjusts styling and refactors how the banner component is exported. Updates the Button component import for consistency with the new structure.

--- components/ui/navbar/navigation-banner.tsx
+++ components/ui/navbar/navigation-banner.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 import { useUserRole } from '@/lib/utils';
-import { Button } from './button';
+
+import { Button } from '@/components/ui/button';
 
 const Banner = () => {
   const userRole = useUserRole();
@@ -13,26 +15,26 @@ const Banner = () => {
   const adminList = ['admin', 'super_user', 'instructor'];
 
   return (
-    <div className={'block w-full bg-gray shadow-sm'}>
+    <div className={'block w-full bg-muted shadow-sm'}>
       <div className='mx-auto max-w-screen-2xl py-2 px-6'>
         <ul
           className={`flex  ${
             adminList?.includes(`${role}`) ? 'justify-end' : 'justify-start'
-          } gap-4 text-maroon md:justify-end`}
+          } gap-4 text-white md:justify-end`}
         >
           {adminList?.includes(`${role}`) ? (
             <>
               {pathname === '/dashboard/instructor' ? (
                 <li>
-                  <Button size={'sm'}>
-                    <Link href='/courses' className='uppercase'>
+                  <Button variant={'action'} size={'sm'}>
+                    <Link href='/catalog' className='uppercase'>
                       Exit Instructor Mode
                     </Link>
                   </Button>
                 </li>
               ) : (
                 <li>
-                  <Button size={'sm'}>
+                  <Button variant={'action'} size={'sm'}>
                     <Link href='/dashboard/instructor' className='uppercase'>
                       Instructor Mode
                     </Link>
@@ -48,10 +50,7 @@ const Banner = () => {
                 </Link>
               </li>
               <li>
-                <Link
-                  href='https://www.codedevils.io/support'
-                  className='*:p-0'
-                >
+                <Link href='https://www.codevera.org/support' className='*:p-0'>
                   <Button variant={'link'}>Get Support </Button>
                 </Link>
               </li>
@@ -63,4 +62,4 @@ const Banner = () => {
   );
 };
 
-export default Banner;
+export { Banner };
